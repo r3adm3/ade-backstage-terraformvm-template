aeab7273fd025b19bdf09ffa64fadd7b1c046cce
Daed <adrianjfarnell@gmail.com>
2023-05-02 20:52:19 +0100

    Update providers.tf

--- content/providers.tf
+++ content/providers.tf
@@ -21,7 +21,7 @@ provider "azurerm" {
   features {}
 
   subscription_id = "8a9a3ad6-9b54-43ca-9458-00c3dc87494f"
-  client_id       = var.client_id
-  client_secret   = var.client_secret
+  client_id       = "changeme"
+  client_secret   = "changeme"
   tenant_id       = "cada6dcc-8116-459d-8d12-7f0a587f8069"
 }
